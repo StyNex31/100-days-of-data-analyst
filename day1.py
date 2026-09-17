f8c7a7ef6c4158186bb367c3983d744f46bb69ae
StyNex31 <n6ntt5ywzh@privaterelay.appleid.com>
2026-09-18 03:46:37 +0500

    день первый начало

---
+++ day1.py
@@ -0,0 +1,18 @@
+name = "твоё имя"
+age = 20
+print(f"Меня зовут {name}, мне {age} лет")
+
+name = 'Tima'
+age = 19
+print(f'Меня зовут {name}, мне {age} лет')
+
+
+
+
+product = 'Ноутбук'
+price = 450000
+discount = 15
+
+print(f"Товар {product}, цена: {price} , скидка: {discount}% , цена со скидкой: {price*discount} " )
+
+
