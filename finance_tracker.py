905ee09e29243c5cc110b2389981c273dfb0d910
StyNex31 <n6ntt5ywzh@privaterelay.appleid.com>
2026-09-20 02:56:24 +0500

    day 3: conditionals

--- finance_tracker.py
+++ finance_tracker.py
@@ -16,4 +16,36 @@ print(expenses[0])   # 1500 — первый элемент
 print(expenses[1])   # 3000 — второй элемент
 print(expenses[-1])  # 800 — последний элемент (удобно для "последняя трата")
 
+expenses = [1500, 3000, 500, 12000, 800]
+
+for expense in expenses:
+    if expense < 500:
+        print("Мелкая трата:", expense)
+    elif expense <= 1500:
+        print("Обычная трата:", expense)
+    else:
+        print("Крупная покупка:", expense)
+
+# Логика если меньше 500 мелкая иначе если ≤1500 обычная(не нужно снова писать >= 500 потому что если бы было меньше 500 сработал бы уже if) иначе (то есть больше 1500) крупная
+
+for expense in expenses:
+    if expense > 1000:
+        print("Крупная покупка:", expense)
+    else:
+        print("Обычная трата:", expense)
+
+
+expense = [1000, 3333, 222, 55555, 1332, 444, 120, 12]
+
+for expense in expenses:
+    if expense > 1500:
+        print("Крупная покупка:", expense)
+    elif expense > 500:
+        print("Обычная трата:", expense)
+    else:
+        print("Мелкая трата:", expense)
+
+
+
+
 
